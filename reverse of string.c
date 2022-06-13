067d4771e7ed781d47c37ae645d9af95d3963387
annemashokreddy <107038429+annemashokreddy@users.noreply.github.com>
2022-06-13 17:36:06 +0530

    Update reverse of string.c

--- reverse of string.c
+++ reverse of string.c
@@ -2,20 +2,17 @@
 #include<string.h>
 int main(void)
 {
-    char mystrg[10];
+    char strg[25];
     int leng, i;
 
-    // Printing the program name and what the program will do
     printf("Program in C for reversing a given string \n ");
-
-    // fetch the input string from the user
-    scanf( "%s", mystrg );
+    scanf( "%s", strg );
 
     // This will find the length of your string with the help of strlen() function of string.h header file
-    leng = strlen(mystrg);
+    leng = strlen(strg);
     for(i = leng - 1; i >= 0; i--) 
     {
-        printf("%c", mystrg[i]);        
+        printf("%c", strg[i]);        
     }
     return 0;
 }
