7a8a228d958033abf32c0204ebdd7b4bf66305b7
annemashokreddy <107038429+annemashokreddy@users.noreply.github.com>
2022-06-15 11:38:16 +0530

    Create flip.c

---
+++ flip.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n, flipNum;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    flipNum =n;
+    printf("Actual number = %d", n);
+    printf("Value after flipping the bits = %d ", flipNum);
+
+    return 0;
+}
