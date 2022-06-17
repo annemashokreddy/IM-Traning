09c85b918db3e0b4b241786c73b97a0b3dd8f843
annemashokreddy <107038429+annemashokreddy@users.noreply.github.com>
2022-06-17 12:30:28 +0530

    Create pascal.c

---
+++ pascal.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int rows, space, i, j,k;
+printf("Enter the number of rows :");
+scanf("%d",&rows);
+for(i=0; i<rows; i++)
+{
+for(space=0; space<= rows-i; ++space)
+printf("  ");
+for(j=0; j <= i; j++)
+{
+if (j==0 || i==0)
+k= 1;
+else
+k = k*(i-j+1)/j;
+printf("%3d", k);
+}
+printf("\n");
+}
+return 0;
+}
