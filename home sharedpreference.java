690376f19c3feb5f551516a76107fcd2ea4b5e16
annemashokreddy <107038429+annemashokreddy@users.noreply.github.com>
2022-07-13 15:31:01 +0530

    Create home sharedpreference.java

---
+++ home sharedpreference.java
@@ -0,0 +1,50 @@
+package com.example.sharedservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+    TextView textView_name,textView_email;
+     Button button_logout;
+    SharedPreferences sharedPreferences;
+    private static final String SHARED_PREF_NAME="mypref";
+    private static final String KEY_NAME="name";
+    private static final String KEY_EMAIL="email";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        textView_email=findViewById(R.id.text_email);
+        textView_name=findViewById(R.id.edittext_name);
+        button_logout=findViewById(R.id.nutton_logout);
+
+        sharedPreferences=getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
+        String name=sharedPreferences.getString(KEY_NAME,null);
+        String email=sharedPreferences.getString(KEY_EMAIL,null);
+
+        if (name!=null ||email!=null){
+            //so set the data on textview
+            textView_name.setText("Full Name-"+name);
+            textView_email.setText("Email ID-"+email);
+        }//so cll to button logout
+
+        button_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor=sharedPreferences.edit();
+                editor.clear();
+                editor.commit();
+                Toast.makeText(HomeActivity.this,"Log Out success",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+}
